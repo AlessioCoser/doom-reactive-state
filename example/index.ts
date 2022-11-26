360053c16a8cb70af4885c6ea191a20ddb6c1a02
Alessio Coser <>
2022-11-26 22:09:17 +0100

    functional trial

--- example/index.ts
+++ example/index.ts
@@ -11,7 +11,7 @@ function useState<T>(initial: T): [Accessor<T>, Setter<T>] {
   return (function () {
     var state: T = initial
     const accessor: Accessor<T> = function accessor() {
-      console.log('accessor ctx', currentObserver?.id, currentObserver?.fn)
+      // console.log('accessor ctx', currentObserver?.id)
       if(currentObserver && toBeObserved(currentObserver)) {
         observers.push(currentObserver)
       }
@@ -20,8 +20,7 @@ function useState<T>(initial: T): [Accessor<T>, Setter<T>] {
     }
 
     const setter: Setter<T> = function setter(v: T): void {
-      console.log('accessor ctx', currentObserver?.id, currentObserver?.fn, currentObserver?.deferred)
-      console.log('observers', observers)
+      // console.log('accessor ctx', currentObserver?.id, currentObserver?.deferred)
       state = v
       observers.forEach(({id, fn}) => {
         if(!currentObserver) {
@@ -38,41 +37,96 @@ function useState<T>(initial: T): [Accessor<T>, Setter<T>] {
   })()
 }
 
-type Observer = {id: string, fn: () => void, deferred: (() => void)[]}
+type FC = (...x: any) => HTMLElement
+
+type Observer = {id: string, fn: FC, deferred: (() => void)[]}
 let currentObserver: Observer | null = null
-const r: <T>(f: T) => T = (function r() {
+let previousObserver: Observer | null = null
+
+const r: FC = (function r() {
   let rIndex: number = 0
 
-  return function r<T>(f: T): T {
+  return function r(component: FC): HTMLElement {
     rIndex += 1
-    const id = `${rIndex}-${(f as Function).name}`
-    const s: Observer = { id: id, fn: () => {}, deferred: [] }
-    return (function(...args: any[]) {
-      s.fn = () => {
-        s.deferred = []
-        currentObserver = s;
-        (f as Function)(...args)
-        const d = currentObserver.deferred
-        currentObserver = null
-        d.forEach(fn => fn())
-      }
-      s.fn()
-    } as T)
+    const id = `${rIndex}-${component.name}`
+    const s: Observer = {
+      id,
+      fn() {
+        this.deferred = []
+        previousObserver = currentObserver
+        currentObserver = this;
+        const result = component.bind({id})()
+        const deferred = currentObserver.deferred
+        currentObserver = previousObserver
+        deferred.forEach(fn => fn())
+        return result
+      },
+      deferred: []
+    }
+    return s.fn()
   }
 })()
 
+type Prop<T> = () => T | T
+type HtmlAction = (ev: MouseEvent) => Promise<void> | void
 
-function h(val: Accessor<string>, increase: Function){
-  console.log(`rendered ${val()}`)
-  increase()
+type HOptions = { text: Prop<string>, onclick: HtmlAction }
+const h: FC = ({text, onclick}): HTMLElement => {
+  return r(function h () {
+    const el = getOrCreate(this.id, "button") as HTMLButtonElement
+    el.innerText = evaluate(text)
+    el.onclick = async (e) => {
+      el.innerText = 'loading'
+      el.disabled=true
+      await onclick(e)
+      el.innerText = evaluate(text)
+      el.disabled=false
+    }
+    return el
+  })
 }
 
-function count({ num }: {num: Accessor<number>}) {
-  console.log('=>', num())
+type CountOptions = { text: Prop<string> }
+function count({ text }: CountOptions) : HTMLElement{
+  return r(function count() {
+    const el = getOrCreate(this.id, "h2")
+    el.innerText = evaluate(text)
+    return el
+  })
+}
+
+type DivOptions = { children: HTMLElement[] }
+function div({children}: DivOptions): HTMLElement {
+  return r(function div() {
+    const el = getOrCreate(this.id, "div")
+    children.forEach(child => el.appendChild(child))
+    return el
+  })
+}
+
+function root(child: () => HTMLElement) {
+  document.body.appendChild(child())
+}
+
+function evaluate<T>(prop: Prop<T>): T {
+  if(typeof prop === 'function'){
+    return prop()
+  }
+  return prop
+}
+
+function createElement(id:string, tag:string): HTMLElement {
+  const element = document.createElement(tag)
+  element.setAttribute('_id', id)
+  return element
+}
+
+function getOrCreate(id:string, tag:string): HTMLElement {
+  return document.querySelector(`[_id="${id}"]`) || createElement(id, tag)
 }
 
 function main () {
-  // use state should be out of rerender loop
+  // use state must be out of rerender loop
   const [val, setter] = useState('initial')
   const [two, setTwo] = useState(0)
 
@@ -80,15 +134,26 @@ function main () {
     setTwo(two() + 1)
   }
 
-  // also functions inside objects are binded
-  r(count)({num: two})
+  const asyncIncrease = async () => {
+    return new Promise((resolve) => {
+      setTimeout(()=>{
+        increase()
+        resolve(null)
+      }, 5000)
+    })
+  }
 
-  // You can't avoid reacting on a used state change
-  r(h)(val, increase)
+  setTimeout(() => setter('updated'), 2000)
 
-  setter('updated')
+  setTimeout(increase, 5000)
 
-  setTimeout(() => setTwo(two() + 1), 1000)
+  return div({children:[
+    // only functions inside objects are binded
+    // all computed properties must be functions
+    count({text: () => `count ${two()}`}),
+    // to avoid reacting you should not wrap text value in lambda you should handle it in h
+    h({text: `render ${val()}`, onclick: asyncIncrease})
+  ]})
 }
 
-main()
+root(main)

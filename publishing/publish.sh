acb4ade246d2d409786618bae8076766f39bb806
Alessio Coser <>
2022-12-07 11:45:01 +0100

    remove unnecessary sed cereated package.json-e file from published package

--- publishing/publish.sh
+++ publishing/publish.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 cp "$(pwd)/publishing/package.json" "$(pwd)/dist/"
+cp "$(pwd)/publishing/.npmignore" "$(pwd)/dist/"
 sed -i'' -e "s|%VERSION%|$1|" "$(pwd)/dist/package.json"
 cd $(pwd)/dist/
 echo "... Publishing version $1 ..."

239d1dc92b4226b29bdb8e86892b6cb7582cdeef
Alessio Coser <>
2023-02-01 16:14:45 +0100

    use a div element with properties

--- example/index.ts
+++ example/index.ts
@@ -1,1 +1,6 @@
-alert('changeme')
+import { h } from '../src/dom'
+// we always return a DOM Element from our components
+
+const component = h("div", { onclick: () => alert('alert') }, ["test"])
+
+document.body.appendChild(component)

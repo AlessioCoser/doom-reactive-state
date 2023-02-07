fc28836cecbc1d66d6b07ecd0dfd1a450c34826a
Alessio Coser <>
2023-02-07 11:58:56 +0100

    omit readonly properties

--- src/dom.ts
+++ src/dom.ts
@@ -1,13 +1,20 @@
 import { effect } from "./reactivity"
 import { updateChildren } from './updateChildren'
 
-type Child = Element | string
-type Reactive<T> = T | (() => T)
 const pass = <T>(prop: Reactive<T>): T => prop as T
 const evaluate = <T>(prop: Reactive<T>): T => typeof prop !== 'function' ? prop : (prop as Function)()
-type Properties<T extends keyof HTMLElementTagNameMap> = Partial<Omit<{
+
+type Child = Element | string
+type Reactive<T> = T | (() => T)
+
+type NonFunctionPropertyNames<T> = { [K in keyof T]: T[K] extends Function ? never : K }[keyof T]
+type NonFunctionProperties<T> = Pick<T, NonFunctionPropertyNames<T>>
+type PartialWithStyles<T> = Partial<{ style: Reactive<Styles> } & Omit<T, 'style'>>
+type WritableProperties<T extends keyof HTMLElementTagNameMap> = Omit<{
   [K in keyof HTMLElementTagNameMap[T] as K extends keyof HTMLElementTagNameMap[T] ? K : never]: Reactive<HTMLElementTagNameMap[T][K]>
-}, 'style'> & { style: Reactive<Styles> }>
+}, 'ATTRIBUTE_NODE' | 'CDATA_SECTION_NODE' | 'COMMENT_NODE' | 'DOCUMENT_FRAGMENT_NODE' | 'DOCUMENT_NODE' | 'DOCUMENT_POSITION_CONTAINED_BY' | 'DOCUMENT_POSITION_CONTAINS' | 'DOCUMENT_POSITION_DISCONNECTED' | 'DOCUMENT_POSITION_FOLLOWING' | 'DOCUMENT_POSITION_IMPLEMENTATION_SPECIFIC' | 'DOCUMENT_POSITION_PRECEDING' | 'DOCUMENT_TYPE_NODE' | 'ELEMENT_NODE' | 'ENTITY_NODE' | 'ENTITY_REFERENCE_NODE' | 'NOTATION_NODE' | 'PROCESSING_INSTRUCTION_NODE' | 'TEXT_NODE' | 'accessKeyLabel' | 'assignedSlot' | 'attributes' | 'baseURI' | 'childElementCount' | 'childNodes' | 'children' | 'classList' | 'clientHeight' | 'clientLeft' | 'clientTop' | 'clientWidth' | 'dataset' | 'firstChild' | 'firstElementChild' | 'isConnected' | 'isContentEditable' | 'lastChild' | 'lastElementChild' | 'localName' | 'namespaceURI' | 'nextElementSibling' | 'nextSibling' | 'nodeName' | 'nodeType' | 'offsetHeight' | 'offsetLeft' | 'offsetParent' | 'offsetTop' | 'offsetWidth' | 'ownerDocument' | 'parentElement' | 'parentNode' | 'part' | 'prefix' | 'previousElementSibling' | 'previousSibling' | 'scrollHeight' | 'scrollWidth' | 'shadowRoot' | 'tagName' | 'ATTRIBUTE_NODE' | 'CDATA_SECTION_NODE' | 'COMMENT_NODE' | 'DOCUMENT_FRAGMENT_NODE' | 'DOCUMENT_NODE' | 'DOCUMENT_POSITION_CONTAINED_BY' | 'DOCUMENT_POSITION_CONTAINS' | 'DOCUMENT_POSITION_DISCONNECTED' | 'DOCUMENT_POSITION_FOLLOWING' | 'DOCUMENT_POSITION_IMPLEMENTATION_SPECIFIC' | 'DOCUMENT_POSITION_PRECEDING' | 'DOCUMENT_TYPE_NODE' | 'ELEMENT_NODE' | 'ENTITY_NODE' | 'ENTITY_REFERENCE_NODE' | 'NOTATION_NODE' | 'PROCESSING_INSTRUCTION_NODE' | 'TEXT_NODE' | 'accessKeyLabel' | 'assignedSlot' | 'attributes' | 'baseURI' | 'childElementCount' | 'childNodes' | 'children' | 'classList' | 'clientHeight' | 'clientLeft' | 'clientTop' | 'clientWidth' | 'dataset' | 'firstChild' | 'firstElementChild' | 'isConnected' | 'isContentEditable' | 'lastChild' | 'lastElementChild' | 'localName' | 'namespaceURI' | 'nextElementSibling' | 'nextSibling' | 'nodeName' | 'nodeType' | 'offsetHeight' | 'offsetLeft' | 'offsetParent' | 'offsetTop' | 'offsetWidth' | 'ownerDocument' | 'parentElement' | 'parentNode' | 'part' | 'prefix' | 'previousElementSibling' | 'previousSibling' | 'scrollHeight' | 'scrollWidth' | 'shadowRoot' | 'style' | 'tagName'>
+
+type Properties<T extends keyof HTMLElementTagNameMap> = PartialWithStyles<NonFunctionProperties<WritableProperties<T>>>
 
 type StylesDeclaration = Omit<CSSStyleDeclaration, typeof Symbol.iterator | number | 'length' | 'parentRule' | 'getPropertyPriority' | 'getPropertyValue' | 'removeProperty' | 'setProperty' | 'item'>
 type Styles = { [K in keyof StylesDeclaration as K extends keyof StylesDeclaration ? K : never]?: Reactive<StylesDeclaration[K]> }

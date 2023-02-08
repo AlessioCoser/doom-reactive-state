1722a7cb6d25b96fd4515f757cd6d531da6d2bd6
Alessio Coser <>
2023-02-08 12:01:03 +0100

    move down dom types

--- src/dom.ts
+++ src/dom.ts
@@ -1,37 +1,6 @@
 import { effect } from "./reactivity"
 import { updateChildren } from './updateChildren'
 
-const pass = <T>(prop: Reactive<T>): T => prop as T
-const evaluate = <T>(prop: Reactive<T>): T => typeof prop !== 'function' ? prop : (prop as Function)()
-
-type Child = Element | string
-type Reactive<T> = T | (() => T)
-
-type IfEquals<X, Y, A, B> = (<T>() => T extends X ? 1 : 2) extends (<T>() => T extends Y ? 1 : 2) ? A : B
-type WritableKeysOf<T> = {[P in keyof T]: IfEquals<{ [Q in P]: T[P] }, { -readonly [Q in P]: T[P] }, P, never>}[keyof T]
-type WritablePart<T> = Pick<T, WritableKeysOf<T>>;
-
-type FunctionPropertyNames<T> = { [K in keyof T]: T[K] extends Function ? K : never }[keyof T]
-
-type WritableCSSStyleDeclaration = WritablePart<CSSStyleDeclaration>
-type StylesDeclaration = Omit<WritableCSSStyleDeclaration, FunctionPropertyNames<WritableCSSStyleDeclaration>>
-type Styles = { [K in keyof StylesDeclaration as K extends keyof StylesDeclaration ? K : never]?: Reactive<StylesDeclaration[K]> }
-type Style = {key: keyof Styles, value: Styles[keyof Styles]}
-type PartialWithStyles<T> = Partial<T & { style: Reactive<Styles> }>
-
-type ElementWithoutEvents<T extends keyof HTMLElementTagNameMap> = Omit<HTMLElementTagNameMap[T], keyof GlobalEventHandlers | FunctionPropertyNames<Element>>
-type ElementProperties<T extends keyof HTMLElementTagNameMap> = PartialWithStyles<WritablePart<ElementWithoutEvents<T>>>
-type ElementEvents = Partial<Omit<GlobalEventHandlers, FunctionPropertyNames<Element>>>
-
-type Properties<T extends keyof HTMLElementTagNameMap> = {
-  [K in keyof ElementProperties<T> as K extends keyof HTMLElementTagNameMap[T] ? K : never]?: Reactive<ElementProperties<T>[K]>
-} & ElementEvents
-
-type Property<T extends keyof HTMLElementTagNameMap> = {
-  key: keyof HTMLElementTagNameMap[T],
-  value: Reactive<HTMLElementTagNameMap[T][keyof HTMLElementTagNameMap[T]]>
-}
-
 export function h<K extends keyof HTMLElementTagNameMap>(tag: K, properties: Properties<K> = {}, children: Reactive<Child[]> = []): HTMLElementTagNameMap[K] {
   const el: HTMLElementTagNameMap[K] = document.createElement(tag)
 
@@ -84,4 +53,35 @@ function toChildNode(child: Child): ChildNode {
   return (typeof child === 'string') ?
       document.createTextNode(child)
       : child
+}
+
+const pass = <T>(prop: Reactive<T>): T => prop as T
+const evaluate = <T>(prop: Reactive<T>): T => typeof prop !== 'function' ? prop : (prop as Function)()
+
+type Child = Element | string
+type Reactive<T> = T | (() => T)
+
+type IfEquals<X, Y, A, B> = (<T>() => T extends X ? 1 : 2) extends (<T>() => T extends Y ? 1 : 2) ? A : B
+type WritableKeysOf<T> = {[P in keyof T]: IfEquals<{ [Q in P]: T[P] }, { -readonly [Q in P]: T[P] }, P, never>}[keyof T]
+type WritablePart<T> = Pick<T, WritableKeysOf<T>>;
+
+type FunctionPropertyNames<T> = { [K in keyof T]: T[K] extends Function ? K : never }[keyof T]
+
+type WritableCSSStyleDeclaration = WritablePart<CSSStyleDeclaration>
+type StylesDeclaration = Omit<WritableCSSStyleDeclaration, FunctionPropertyNames<WritableCSSStyleDeclaration>>
+type Styles = { [K in keyof StylesDeclaration as K extends keyof StylesDeclaration ? K : never]?: Reactive<StylesDeclaration[K]> }
+type Style = {key: keyof Styles, value: Styles[keyof Styles]}
+type PartialWithStyles<T> = Partial<T & { style: Reactive<Styles> }>
+
+type ElementWithoutEvents<T extends keyof HTMLElementTagNameMap> = Omit<HTMLElementTagNameMap[T], keyof GlobalEventHandlers | FunctionPropertyNames<Element>>
+type ElementProperties<T extends keyof HTMLElementTagNameMap> = PartialWithStyles<WritablePart<ElementWithoutEvents<T>>>
+type ElementEvents = Partial<Omit<GlobalEventHandlers, FunctionPropertyNames<Element>>>
+
+type Properties<T extends keyof HTMLElementTagNameMap> = {
+  [K in keyof ElementProperties<T> as K extends keyof HTMLElementTagNameMap[T] ? K : never]?: Reactive<ElementProperties<T>[K]>
+} & ElementEvents
+
+type Property<T extends keyof HTMLElementTagNameMap> = {
+  key: keyof HTMLElementTagNameMap[T],
+  value: Reactive<HTMLElementTagNameMap[T][keyof HTMLElementTagNameMap[T]]>
 }

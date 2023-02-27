9aa347e9c0579d33448b9f10d57b3b2df6e0baf7
Alessio Coser <>
2023-02-27 01:03:01 +0100

    remove unused vite plugins

--- vite.config.ts
+++ vite.config.ts
@@ -1,36 +1,6 @@
 import { defineConfig } from 'vitest/config'
-import dts from "vite-plugin-dts"
-import { viteStaticCopy } from 'vite-plugin-static-copy'
 
 export default defineConfig({
-  plugins: [
-    dts({
-      insertTypesEntry: true,
-    }),
-    // viteStaticCopy({
-    //   targets: [
-    //     { src: 'src/dom/jsx.d.ts', dest: 'dom' },
-    //   ]
-    // })
-  ],
-  build: {
-    lib: {
-      entry: './src',
-      fileName: 'index',
-      // formats: ['es', 'cjs']
-    },
-    // rollupOptions: {
-    //   input: [
-    //     './src/index.ts',
-    //     './src/dom/jsx-dev-runtime.ts',
-    //     './src/dom/jsx-runtime.ts',
-    //   ],
-    //   output: {
-    //     preserveModules: false,
-    //     inlineDynamicImports: false,
-    //   }
-    // }
-  },
   test: {
     globals: true,
     environment: 'jsdom',

d3f4054fa45bcb142cad3d8025eb7d66d3094b05
Alessio Coser <18616553+AlessioCoser@users.noreply.github.com>
2024-03-23 22:24:16 +0100

    do not use it.each: it is not so well integrated with VSCode

--- test/dom/updateChildren.test.ts
+++ test/dom/updateChildren.test.ts
@@ -9,83 +9,76 @@ describe("update children", () => {
   })
 
   describe("with Element items", () => {
-    it.each([
-      ["no changes", [ p(1) ], [ p(1) ]],
-      ["append from empty", [  ], [ p(1) ]],
-      ["append element", [ p(1) ], [ p(1), p(2) ]],
-      ["append multiple element", [ p(1) ], [ p(1), p(2), p(3) ]],
-      ["prepend element", [ p(1) ], [ p(2), p(1) ]],
-      ["prepend multiple element", [ p(1) ], [ p(3), p(2), p(1) ]],
-      ["prepend middle element", [ p(1), p(2) ], [ p(1), p(3), p(2) ]],
-      ["remove child", [ p(1) ], [  ]],
-      ["remove multiple children (equals at the start)", [ p(1), p(1.5), p(2) ], [ p(1) ]],
-      ["remove multiple children (equals at the end)", [ p(1), p(1.5), p(2) ], [ p(2) ]],
-      ["remove multiple child (equals in the middle)", [ p(1), p(2), p(3) ], [ p(2) ]],
-      ["swap first and last nodes", [ p(1), p(2) ], [ p(2), p(1) ]],
-      ["swap first and last nodes with 2 in the middle", [ p(1), p(2), p(3), p(4) ], [ p(4), p(2), p(3), p(1) ]],
-      ["replace child", [ p(1), p(2) ], [ p(1), p(3) ]],
-      ["duplicated nodes", [ p(1), p(2), p(3), p(2) ], [ p(2), p(1), p(2), p(3) ]],
-      ["complete example", [ p(1), p(2), p(3), p(4), p(5) ], [ p(2), p(5), p(1), p(6) ]]
-    ])('%p', (_: string, asIs: ChildNode[], toBe: ChildNode[]) => {
-      appendChildren(body, asIs)
-      testUpdateChildren(body, toBe)
-    });
+    it("no changes", testSingle([ p(1) ], []))
+    it("append from empty", testSingle([  ], [ p(1) ]))
+    it("append element", testSingle([ p(1) ], [ p(1), p(2) ]))
+    it("append multiple element", testSingle([ p(1) ], [ p(1), p(2), p(3) ]))
+    it("prepend element", testSingle([ p(1) ], [ p(2), p(1) ]))
+    it("prepend multiple element", testSingle([ p(1) ], [ p(3), p(2), p(1) ]))
+    it("prepend middle element", testSingle([ p(1), p(2) ], [ p(1), p(3), p(2) ]))
+    it("remove child", testSingle([ p(1) ], [  ]))
+    it("remove multiple children (equals at the start)", testSingle([ p(1), p(1.5), p(2) ], [ p(1) ]))
+    it("remove multiple children (equals at the end)", testSingle([ p(1), p(1.5), p(2) ], [ p(2) ]))
+    it("remove multiple child (equals in the middle)", testSingle([ p(1), p(2), p(3) ], [ p(2) ]))
+    it("swap first and last nodes", testSingle([ p(1), p(2) ], [ p(2), p(1) ]))
+    it("swap first and last nodes with 2 in the middle", testSingle([ p(1), p(2), p(3), p(4) ], [ p(4), p(2), p(3), p(1) ]))
+    it("replace child", testSingle([ p(1), p(2) ], [ p(1), p(3) ]))
+    it("duplicated nodes", testSingle([ p(1), p(2), p(3), p(2) ], [ p(2), p(1), p(2), p(3) ]))
+    it("complete example", testSingle([ p(1), p(2), p(3), p(4), p(5) ], [ p(2), p(5), p(1), p(6)]))
   })
 
   describe("with Text items", () => {
-    it.each([
-      ["no changes", [ t(1) ], [ t(1) ]],
-      ["append from empty", [  ], [ t(1) ]],
-      ["append element", [ t(1) ], [ t(1), t(2) ]],
-      ["append multiple element", [ t(1) ], [ t(1), t(2), t(3) ]],
-      ["prepend element", [ t(1) ], [ t(2), t(1) ]],
-      ["prepend multiple element", [ t(1) ], [ t(3), t(2), t(1) ]],
-      ["prepend middle element", [ t(1), t(2) ], [ t(1), t(3), t(2) ]],
-      ["remove child", [ t(1) ], [  ]],
-      ["first empty to filled", [ empty(), t(2) ], [ t(1), t(2) ]],
-      ["last empty to filled", [ t(1), empty() ], [ t(1), t(2) ]],
-      ["first filled to empty", [ t(1), t(2) ], [ empty(), t(2) ]],
-      ["last filled to empty", [ t(1), t(2) ], [ t(1), empty() ]],
-      ["remove multiple children (equals at the start)", [ t(1), t(1.5), t(2) ], [ t(1) ]],
-      ["remove multiple children (equals at the end)", [ t(1), t(1.5), t(2) ], [ t(2) ]],
-      ["remove multiple child (equals in the middle)", [ t(1), t(2), t(3) ], [ t(2) ]],
-      ["swap first and last nodes", [ t(1), t(2) ], [ t(2), t(1) ]],
-      ["swap first and last nodes with 2 in the middle", [ t(1), t(2), t(3), t(4) ], [ t(4), t(2), t(3), t(1) ]],
-      ["replace child", [ t(1), t(2) ], [ t(1), t(3) ]],
-      ["duplicated nodes", [ t(1), t(2), t(3), t(2) ], [ t(2), t(1), t(2), t(3) ]],
-      ["complete example", [ t(1), t(2), t(3), t(4), t(5) ], [ t(2), t(5), t(1), t(6) ]]
-    ])('%p', (_: string, asIs: ChildNode[], toBe: ChildNode[]) => {
-      appendChildren(body, asIs)
-      testUpdateChildren(body, toBe)
-    });
+    it("no changes", testSingle([ t(1) ], [ t(2), t(1)  ]))
+    it("append from empty", testSingle([  ], [ t(1) ]))
+    it("append element", testSingle([ t(1) ], [ t(1), t(2) ]))
+    it("append multiple element", testSingle([ t(1) ], [ t(1), t(2), t(3) ]))
+    it("prepend element", testSingle([ t(1) ], [ t(2), t(1) ]))
+    it("prepend multiple element", testSingle([ t(1) ], [ t(3), t(2), t(1) ]))
+    it("prepend middle element", testSingle([ t(1), t(2) ], [ t(1), t(3), t(2) ]))
+    it("remove child", testSingle([ t(1) ], [  ]))
+    it("first empty to filled", testSingle([ empty(), t(2) ], [ t(1), t(2) ]))
+    it("last empty to filled", testSingle([ t(1), empty() ], [ t(1), t(2) ]))
+    it("first filled to empty", testSingle([ t(1), t(2) ], [ empty(), t(2) ]))
+    it("last filled to empty", testSingle([ t(1), t(2) ], [ t(1), empty() ]))
+    it("remove multiple children (equals at the start)", testSingle([ t(1), t(1.5), t(2) ], [ t(1) ]))
+    it("remove multiple children (equals at the end)", testSingle([ t(1), t(1.5), t(2) ], [ t(2) ]))
+    it("remove multiple child (equals in the middle)", testSingle([ t(1), t(2), t(3) ], [ t(2) ]))
+    it("swap first and last nodes", testSingle([ t(1), t(2) ], [ t(2), t(1) ]))
+    it("swap first and last nodes with 2 in the middle", testSingle([ t(1), t(2), t(3), t(4) ], [ t(4), t(2), t(3), t(1) ]))
+    it("replace child", testSingle([ t(1), t(2) ], [ t(1), t(3) ]))
+    it("duplicated nodes", testSingle([ t(1), t(2), t(3), t(2) ], [ t(2), t(1), t(2), t(3) ]))
+    it("complete example", testSingle([ t(1), t(2), t(3), t(4), t(5) ], [ t(2), t(5), t(1), t(6) ]))
   })
 
   describe("with Text and Element items", () => {
-    it.each([
-      ["mixed", [ p(1), t(2), p(3), t(4), t(5) ], [ t(5), p(1), t(6), p(6) ]]
-    ])('%p', (_: string, asIs: ChildNode[], toBe: ChildNode[]) => {
-      appendChildren(body, asIs)
-      testUpdateChildren(body, toBe)
-    });
+    it("mixed", testSingle([ p(1), t(2), p(3), t(4), t(5) ], [ t(5), p(1), t(6), p(6) ]))
   })
 
   describe("update two times", () => {
-    it.each([
-      ["first empty to filled to empty", [ empty(), t(2) ], [ t(1), t(2) ], [ empty(), t(2) ]],
-      ["last empty to filled to empty", [ t(1), empty() ], [ t(1), t(2) ], [ t(1), empty() ]],
-      ["first filled to empty to filled", [ t(1), t(2) ], [ empty(), t(2)  ], [ t(1), t(2) ]],
-      ["last filled to empty to filled", [ t(1), t(2) ], [ t(1), empty() ], [ t(1), t(2) ]]
-    ])('%p', (_: string, asIs: ChildNode[], being: ChildNode[], toBe: ChildNode[]) => {
-      appendChildren(body, asIs)
-      testUpdateChildren(body, being)
-      testUpdateChildren(body, toBe)
-    });
+    it("first empty to filled to empty", testDouble([ empty(), t(2) ], [ t(1), t(2) ], [ empty(), t(2) ]))
+    it("last empty to filled to empty", testDouble([ t(1), empty() ], [ t(1), t(2) ], [ t(1), empty() ]))
+    it("first filled to empty to filled", testDouble([ t(1), t(2) ], [ empty(), t(2)  ], [ t(1), t(2) ]))
+    it("last filled to empty to filled", testDouble([ t(1), t(2) ], [ t(1), empty() ], [ t(1), t(2) ]))
   })
 })
 
+function testSingle(asIs: ChildNode[], toBe: ChildNode[]) {
+  return () => {
+    appendChildren(body, asIs)
+    testUpdateChildren(body, toBe)
+  }
+}
+
+function testDouble(asIs: ChildNode[], being: ChildNode[], toBe: ChildNode[]) {
+  return () => {
+    appendChildren(body, asIs)
+    testUpdateChildren(body, being)
+    testUpdateChildren(body, toBe)
+  }
+}
+
 function testUpdateChildren(body: Node, toBe: ChildNode[]) {
   updateChildren(body, toBe)
-
   expect(childrenOf(body)).toEqual(toBe.filter(withoutEmptyTextNodes))
 }
 

90e9632a734da24aa091c99978d34dcbb8e04628
Alessio Coser <18616553+AlessioCoser@users.noreply.github.com>
2024-03-24 22:11:29 +0100

    fix pipeline

--- publish.sh
+++ publish.sh
@@ -4,4 +4,3 @@ sed -i'' -e "s|\"version\": \"%VERSION%\",|\"version\": \"$1\",|" "$(pwd)/packag
 echo "... Publishing version $1 ..."
 npm publish
 sed -i'' -e "s|\"version\": \"$1\",|\"version\": \"%VERSION%\",|" "$(pwd)/package.json"
-rm package.json-e

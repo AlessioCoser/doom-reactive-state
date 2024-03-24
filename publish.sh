1dee6d42844f36cb18feb4ea4ce1509f165850f5
Alessio Coser <18616553+AlessioCoser@users.noreply.github.com>
2024-03-24 22:01:50 +0100

    try to improve package boundling

---
+++ publish.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+sed -i'' -e "s|\"version\": \"%VERSION%\",|\"version\": \"$1\",|" "$(pwd)/package.json"
+echo "... Publishing version $1 ..."
+npm publish
+sed -i'' -e "s|\"version\": \"$1\",|\"version\": \"%VERSION%\",|" "$(pwd)/package.json"
+rm package.json-e
--- vite.config.ts
+++ vite.config.ts
@@ -1,29 +1,11 @@
 import { defineConfig } from "vitest/config";
-import { resolve } from "path";
-import dts from "vite-plugin-dts";
 
 export default defineConfig({
-  build: {
-    sourcemap: true,
-    lib: {
-      name: "DoomReactiveState",
-      entry: resolve(__dirname, "src/index.ts"),
-      formats: ["es", "cjs", "umd"],
-      fileName: 'doom-reactive-state',
-    },
-    target: 'esnext',
-  },
-  resolve: { alias: { src: resolve("src/") } },
   test: {
     globals: true,
     environment: "jsdom",
     coverage: {
       reporter: ["text", "html"],
     },
-  },
-  plugins: [
-    dts({
-      insertTypesEntry: true,
-    }),
-  ],
+  }
 });

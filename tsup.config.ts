3016c19a9875356522538035ba45968cd916fb7b
Alessio Coser <18616553+AlessioCoser@users.noreply.github.com>
2024-03-30 00:27:38 +0100

    add iife and create a release automatically on tag

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,12 +1,29 @@
-import { defineConfig } from 'tsup'
+import { defineConfig } from "tsup";
 
-export default defineConfig({
-  target: 'es2020',
-  format: ['cjs', 'esm'],
-  splitting: false,
-  sourcemap: true,
-  clean: true,
-  minify: true,
-  dts: true,
-  treeshake: true,
-})
+export default defineConfig([
+  {
+    entryPoints: { "index": "src/index.ts" },
+    target: "es2020",
+    format: ["cjs", "esm"],
+    splitting: false,
+    sourcemap: true,
+    clean: true,
+    minify: true,
+    dts: true,
+    treeshake: true,
+    outDir: "dist",
+  },
+  {
+    entryPoints: { "doom-reactive-state": "src/index.ts" },
+    target: "es2020",
+    format: ["iife"],
+    splitting: false,
+    sourcemap: false,
+    clean: true,
+    minify: true,
+    dts: false,
+    treeshake: true,
+    outDir: "cdn",
+    globalName: "doom",
+  },
+]);

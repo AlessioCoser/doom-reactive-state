0cfea879f3cea258acd870293ccb367248e218ef
Alessio Coser <18616553+AlessioCoser@users.noreply.github.com>
2025-01-09 07:55:00 +0100

    export useful types

--- src/dom/index.ts
+++ src/dom/index.ts
@@ -1,2 +1,3 @@
 export * from './h'
 export * from './html'
+export type { Component, HTMLComponent, Children, Child } from "./types";
--- src/reactivity/index.ts
+++ src/reactivity/index.ts
@@ -1,3 +1,4 @@
 export { signal } from "./signal";
 export { effect } from "./effect";
 export { derive, d } from "./derive";
+export type { Accessor, Setter, Signal } from "./types";

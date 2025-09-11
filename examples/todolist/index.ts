caca41ae94a5eba6bbafcacf552557ed176e4be0
Alessio Coser <18616553+AlessioCoser@users.noreply.github.com>
2025-09-11 11:23:40 +0200

    update examples

--- examples/todolist/index.ts
+++ examples/todolist/index.ts
@@ -2,60 +2,61 @@ import { Div, Input, Li, signal, Ul } from "doom-reactive-state"
 import type { Accessor, Component } from "doom-reactive-state"
 
 type Item = { id: number, text: string, done: boolean }
+
 function newItem(text: string, identifier: number | null = null, done: boolean = false): Item {
-  const id = identifier || (new Date()).getTime();
-  return { id, text, done };
+    const id = identifier || (new Date()).getTime();
+    return {id, text, done};
 }
 
 type ItemCallback = (id: Item['id']) => void
 type TodoItemProps = { item: Item, onclick: ItemCallback, onclose: ItemCallback }
-const TodoItem: Component<TodoItemProps> = ({ item, onclick, onclose }) => {
-  const doneClass = () => item.done ? "done" : ""
+const TodoItem: Component<TodoItemProps> = ({item, onclick, onclose}) => {
+    const doneClass = () => item.done ? "done" : ""
 
-  return Li({ className: doneClass, onclick: () => onclick(item.id) }, [
-    Div(() => item.text),
-    Div({ className: "delete", onclick: () => onclose(item.id) }, 'ⓧ')
-  ]);
+    return Li({className: doneClass, onclick: () => onclick(item.id)}, [
+        Div({id: `${item.id}`}, () => item.text),
+        Div({className: "delete", onclick: () => onclose(item.id)}, 'ⓧ')
+    ]);
 }
 
 type TodoListProps = { items: Accessor<Item[]>, onItemClick: ItemCallback, onItemClose: ItemCallback }
-const TodoList: Component<TodoListProps> = ({ items, onItemClick, onItemClose }) => {
-  return Ul(() => items().map((item: Item) =>
-    TodoItem({ item, onclick: onItemClick, onclose: onItemClose })
-  ))
+const TodoList: Component<TodoListProps> = ({items, onItemClick, onItemClose}) => {
+    return Ul(() => items().map((item: Item) =>
+        TodoItem({item, onclick: onItemClick, onclose: onItemClose})
+    ))
 }
 
 const App = () => {
-  const [items, setItems] = signal<Item[]>([
-    newItem("first", 1),
-    newItem("completed", 2, true),
-    newItem("second", 3)
-  ]);
+    const [items, setItems] = signal<Item[]>([
+        newItem("first", 1),
+        newItem("completed", 2, true),
+        newItem("second", 3)
+    ]);
 
-  const onItemClose: ItemCallback = (id) => {
-    setItems(items().filter(item => item.id !== id))
-  }
-
-  const onItemClick: ItemCallback = (id) => {
-    setItems(items().map(item => {
-      if(item.id === id) {
-        return { ...item, done: !item.done }
-      }
-      return item
-    }))
-  }
-
-  const onkeyup = (e) => {
-    if (e.key === "Enter" && e.target.value.trim() !== "") {
-      setItems([newItem(e.target.value.trim()), ...items()])
-      e.target.value = ""
+    const onItemClose: ItemCallback = (id) => {
+        setItems(items().filter(item => item.id !== id))
     }
-  }
 
-  return Div([
-    Input({ type: 'text', onkeyup, placeholder: "Press enter to create" }),
-    TodoList({ items, onItemClick, onItemClose })
-  ])
+    const onItemClick: ItemCallback = (id) => {
+        setItems(items().map(item => {
+            if (item.id === id) {
+                return {...item, done: !item.done}
+            }
+            return item
+        }))
+    }
+
+    const onkeyup = function (this: HTMLInputElement, e: KeyboardEvent) {
+        if (e.key === "Enter" && this.value.trim() !== "") {
+            setItems([newItem(this.value.trim()), ...items()])
+            this.value = ""
+        }
+    }
+
+    return Div([
+        Input({type: 'text', onkeyup, placeholder: "Press enter to create"}),
+        TodoList({items, onItemClick, onItemClose})
+    ])
 }
 
 document.body.appendChild(App())
